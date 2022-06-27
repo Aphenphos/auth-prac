bd03b8b4487225789d38981ccda5784ce149bc7c
Aphenphos <MaxwillWinters@gmail.com>
2022-06-27 15:15:09 -0400

    added form grabbers

--- components/AuthForm.js
+++ components/AuthForm.js
@@ -5,6 +5,11 @@ export default function createAuthForm(form, { handleAuth }) {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
+        const formData = new FormData(form);
+        handleAuth(
+            formData.get('email'),
+            formData.get('password')
+        );
         // *** make form data and call handleAuth with email and password
     });
 
